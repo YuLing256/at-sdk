759ca528a410e75a630422b8092114fa79293659
YuLing <nathan.kb217@gmail.com>
2026-04-16 14:47:23 -0500

    Add TaskDispatcher for main thread task management

---
+++ CSharpSDK/TaskDispatcher.cs
@@ -0,0 +1,40 @@
+#nullable enable
+
+using System.Collections.Concurrent;
+
+namespace CSharpSDK.Utilities;
+
+/// <summary>
+/// This is here to replicate UniTask's SwitchToMainThread
+/// </summary>
+internal static class TaskDispatcher
+{
+	private static readonly ConcurrentQueue<Action> Actions = new();
+	private static int _mainThreadId;
+
+	public static void Initialize()
+	{
+		_mainThreadId = Environment.CurrentManagedThreadId;
+	}
+
+	public static void RunPending()
+	{
+		while (Actions.TryDequeue(out var action)) action();
+	}
+
+	private static bool IsMainThread => Environment.CurrentManagedThreadId == _mainThreadId;
+
+	private static void Post(Action action)
+	{
+		Actions.Enqueue(action);
+	}
+
+	public static Task SwitchToMainThread()
+	{
+		if (IsMainThread) return Task.CompletedTask;
+
+		var tcs = new TaskCompletionSource();
+		Post(() => tcs.SetResult());
+		return tcs.Task;
+	}
+}
